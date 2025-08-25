bc6276185fe5684c292d6aeec108fffcaf844846
Volodymyr Katkalov <volodymyr.katkalov@suse.com>
2025-08-25 14:36:59 +0300

    Intentional break

--- usr/share/lib/img_proof/tests/SLES/EC2/test_sles_ec2_csp_cli.py
+++ usr/share/lib/img_proof/tests/SLES/EC2/test_sles_ec2_csp_cli.py
@@ -10,10 +10,10 @@ def test_sles_ec2_csp_cli(host, is_sle_micro, get_version):
     if version < 16.0:
         pytest.skip('Pre-SLE 16.0 versions do not have CSP CLI.')
 
-    which_cmd = host.run('command -v aws')
+    which_cmd = host.run('command -v aws_x')
     assert which_cmd.rc == 0, f"'{which_cmd.command}' failed with code {which_cmd.rc}."
 
-    aws_cmd = host.run('aws --version')
+    aws_cmd = host.run('aws_x --version')
     assert aws_cmd.rc == 0, f"'{aws_cmd.command}' failed with code {aws_cmd.rc}"
     assert 'aws-cli' in aws_cmd.stdout, \
         "'aws-cli' not found in aws version output"
